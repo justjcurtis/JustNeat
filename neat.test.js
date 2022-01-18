1ade684bebb8b50ab108ffbcafa57545f203ed5e
justjcurtis <justjcurtis@gmail.com>
2022-01-18 22:20:51 +0000

    test-wip: neat.js

---
+++ neat.test.js
@@ -0,0 +1,68 @@
+describe('neat', () => {
+    describe('ctor', () => {
+        // TODO: add tests
+    })
+    describe('FromJson', () => {
+        // TODO: add tests
+    })
+    describe('toJson', () => {
+        // TODO: add tests
+    })
+    describe('reset', () => {
+        // TODO: add tests
+    })
+    describe('getInnovationId', () => {
+        // TODO: add tests
+    })
+    describe('newNode', () => {
+        // TODO: add tests
+    })
+    describe('blankGenome', () => {
+        // TODO: add tests
+    })
+    describe('addConnection', () => {
+        // TODO: add tests
+    })
+    describe('interposeConnection', () => {
+        // TODO: add tests
+    })
+    describe('mcp', () => {
+        // TODO: add tests
+    })
+    describe('dist', () => {
+        // TODO: add tests
+    })
+    describe('crossover', () => {
+        // TODO: add tests
+    })
+    describe('speciate', () => {
+        // TODO: add tests
+    })
+    describe('cull', () => {
+        // TODO: add tests
+    })
+    describe('breed', () => {
+        // TODO: add tests
+    })
+    describe('mutate', () => {
+        // TODO: add tests
+    })
+    describe('populateGenomeCosts', () => {
+        // TODO: add tests
+    })
+    describe('evolve', () => {
+        // TODO: add tests
+    })
+    describe('trainFnStep', () => {
+        // TODO: add tests
+    })
+    describe('trainFn', () => {
+        // TODO: add tests
+    })
+    describe('trainDataStep', () => {
+        // TODO: add tests
+    })
+    describe('trainData', () => {
+        // TODO: add tests
+    })
+})
